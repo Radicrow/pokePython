899d937284922a4def80a9fb32aca2625659dd80
Radicrow <igoracatauassu03@gmail.com>
2024-08-22 19:09:03 -0300

    Second commit :cat:

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
 import pandas as pd
+
+df = pd.read_csv('pokemon_data.csv')
+
+#print(df.columns)
+#print(df[['Name', 'Type 1', 'HP']][0:10])
+
+#print(df.iloc[1])
+
+#for index, row in df.iterrows():
+#    print(row[['Name', 'Type 1']])
+
+print(df.loc[(df['Type 1'] == 'Fire') & (df['Type 2'] == 'Dark')])
+
