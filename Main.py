77678dc3b322f23c9bfd74242d2bff109f89c1e3
Radicrow <igoracatauassu03@gmail.com>
2024-08-31 07:41:04 -0300

    :cat: Adding requirements :cat:

--- Main.py
+++ Main.py
@@ -10,8 +10,20 @@ df = pd.read_csv('pokemon_data.csv')
 #for index, row in df.iterrows():
 #    print(row[['Name', 'Type 1']])
 
-poisontypes = df.loc[(df['Type 1'] == 'Poison') | (df['Type 2'] == 'Poison')]
 
+
+
+df['Total'] = df['Attack'] + df['Defense'] + df['Sp. Atk'] + df['Sp. Def'] + df['Speed'] + df['HP']
+
+df = df.drop(columns=['Total'])
+
+df['Total'] = df.iloc[:, 4:10].sum(axis=1)
+
+poisontypes = df.loc[(df['Type 1'] == 'Poison') | (df['Type 2'] == 'Poison')]
 print(poisontypes)
-poisontypes.to_csv('poisontypes.csv')
+
+
+##poisontypes.to_csv('poisontypes.csv')
+
+
 
